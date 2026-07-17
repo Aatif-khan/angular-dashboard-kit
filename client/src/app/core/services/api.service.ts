1b4c0bc33ef7283081ac591e7f1d407c811e8bab
Aatif-khan <Aatif-khan@users.noreply.github.com>
2026-07-17 17:02:45 +0530

    fix(auth): resolve frontend integration after authentication refactor

--- client/src/app/core/services/api.service.ts
+++ client/src/app/core/services/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
@@ -10,6 +11,13 @@ export class ApiService {
   private http = inject(HttpClient);
   private readonly baseUrl = environment.apiUrl;
 
+  private unwrapResponse<T>(res: any): T {
+    if (res && res.success === true && 'data' in res) {
+      return res.data;
+    }
+    return res;
+  }
+
   get<T>(path: string, params: any = {}): Observable<T> {
     let httpParams = new HttpParams();
     Object.keys(params).forEach(key => {
@@ -17,22 +25,32 @@ export class ApiService {
         httpParams = httpParams.set(key, params[key]);
       }
     });
-    return this.http.get<T>(`${this.baseUrl}/${path}`, { params: httpParams });
+    return this.http.get<any>(`${this.baseUrl}/${path}`, { params: httpParams }).pipe(
+      map(res => this.unwrapResponse<T>(res))
+    );
   }
 
   post<T>(path: string, body: any = {}): Observable<T> {
-    return this.http.post<T>(`${this.baseUrl}/${path}`, body);
+    return this.http.post<any>(`${this.baseUrl}/${path}`, body).pipe(
+      map(res => this.unwrapResponse<T>(res))
+    );
   }
 
   put<T>(path: string, body: any = {}): Observable<T> {
-    return this.http.put<T>(`${this.baseUrl}/${path}`, body);
+    return this.http.put<any>(`${this.baseUrl}/${path}`, body).pipe(
+      map(res => this.unwrapResponse<T>(res))
+    );
   }
 
   delete<T>(path: string): Observable<T> {
-    return this.http.delete<T>(`${this.baseUrl}/${path}`);
+    return this.http.delete<any>(`${this.baseUrl}/${path}`).pipe(
+      map(res => this.unwrapResponse<T>(res))
+    );
   }
 
   patch<T>(path: string, body: any = {}): Observable<T> {
-    return this.http.patch<T>(`${this.baseUrl}/${path}`, body);
+    return this.http.patch<any>(`${this.baseUrl}/${path}`, body).pipe(
+      map(res => this.unwrapResponse<T>(res))
+    );
   }
 }
